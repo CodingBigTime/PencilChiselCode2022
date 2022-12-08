8198bd54c3fff9acd5eb8ae72c2f4b62e588a6c2
LordDeatHunter <desuhansa@gmail.com>
2022-12-09 00:45:59 +0100

    Add logo to main screen

--- PencilChiselCode/Source/GameStates/MenuState.cs
+++ PencilChiselCode/Source/GameStates/MenuState.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace PencilChiselCode.Source.GameStates;
 
 public class MenuState : BonfireGameState
 {
     private readonly List<Button> _buttons = new();
+    private readonly Texture2D _logo;
+    private Vector2 _logoPosition;
+    private const float LogoScale = 2.5F;
 
     public MenuState(Game game) : base(game)
     {
+        _logo = Game.TextureMap["logo"];
+        _logoPosition = new Vector2((Game.GetWindowWidth() - _logo.Width * LogoScale) / 2F, 100);
         BgColor = new Color(9F / 255F, 10F / 255F, 20F / 255F);
     }
 
@@ -39,13 +45,10 @@ public class MenuState : BonfireGameState
 
     public override void Draw(GameTime gameTime)
     {
-        Game.SpriteBatch.Begin();
+        Game.SpriteBatch.Begin(samplerState: SamplerState.PointClamp);
         Game.GraphicsDevice.Clear(BgColor);
-        foreach (var button in _buttons)
-        {
-            button.Draw(Game.SpriteBatch);
-        }
-
+        Game.SpriteBatch.Draw(_logo, _logoPosition, null, Color.White, 0F, Vector2.Zero, LogoScale, SpriteEffects.None, 0F);
+        _buttons.ForEach(button => button.Draw(Game.SpriteBatch));
         Game.SpriteBatch.End();
     }
 
@@ -57,9 +60,6 @@ public class MenuState : BonfireGameState
             return;
         }
 
-        foreach (var button in _buttons)
-        {
-            button.Update(gameTime);
-        }
+        _buttons.ForEach(button => button.Update(gameTime));
     }
 }
